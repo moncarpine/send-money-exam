9f0530c23bacc8fb14f19060e1efbc095a23f0cc
Monty Carlo Pineda <moncarpinew@gmail.com>
2025-12-10 21:53:33 +0800

    Initial Commit

---
+++ SendMoneyExam/SendMoneyExamApp.swift
@@ -0,0 +1,20 @@
+//
+//  SendMoneyExamApp.swift
+//  SendMoneyExam
+//
+//  Created by Monty Carlo Pineda on 12/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SendMoneyExamApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
